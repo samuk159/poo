d7cca29b94c610236fb29488599bc3822df0a914
Samuel Alles Remlinger <samu_65@hotmail.com>
2021-06-27 17:16:01 -0300

    aula 10

---
+++ src/main/java/aula10/NumerosThread.java
@@ -0,0 +1,27 @@
+package aula10;
+
+public class NumerosThread extends Thread {
+    
+    private String nome;
+    private int inicio;
+    private int fim;
+
+    public NumerosThread(String nome, int inicio, int fim) {
+        this.nome = nome;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+    
+    @Override
+    public void run() {
+        for (int i = inicio; i <= fim; i++) {
+            try {
+                System.out.println(nome + ": " + i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
